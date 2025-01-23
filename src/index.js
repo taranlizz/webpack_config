16f4d311b89717a8afc7a74391654feae36e782c
Yelyzaveta Taran <lizataran103@gmail.com>
2025-01-23 17:27:24 +0200

    default html+js project setup

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { sum } from "./test.";
-
-console.log("Hello world");
-console.log(sum(5, 5));
-console.log("Webpack hello");
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (env) => {
   return {
@@ -9,5 +10,10 @@ module.exports = (env) => {
       filename: "[name].[contenthash].js",
       clean: true,
     },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+    ],
   };
 };
