ddc4e51a8bd695f0bd93a3c45f0f450c882c42b9
Yelyzaveta Taran <lizataran103@gmail.com>
2025-01-24 16:19:20 +0200

    added devserver + sourse map tracking

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,14 +1,17 @@
 import path from "path";
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
 type Mode = "development" | "production";
 
 interface EnvVariables {
   mode: Mode;
+  port: number;
 }
 
 export default (env: EnvVariables) => {
+  const isDev = env.mode === "development";
   const config: webpack.Configuration = {
     mode: env.mode ?? "development",
     entry: path.resolve(__dirname, "src", "index.ts"),
@@ -34,6 +37,13 @@ export default (env: EnvVariables) => {
         template: path.resolve(__dirname, "public", "index.html"),
       }),
     ],
+    devtool: isDev && "inline-source-map",
+    devServer: isDev
+      ? {
+          port: env.port ?? 3000,
+          open: true,
+        }
+      : undefined,
   };
   return config;
 };
